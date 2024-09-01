7af5b8106101575f4044fd5213dd2f12d7817430
KIRAN UGALE <141510294+kiranugale2o@users.noreply.github.com>
2024-09-01 13:12:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 const corsAllow = {
-  origin: "*",
-  methods: "PUT,GET,PATCH,DELETE,POST",
-  credentials: false,
+ origin: 'http://localhost:3000', // Frontend URL
+  methods: 'PUT, GET, PATCH, DELETE, POST, OPTIONS',
+  credentials: true, // Allows cookies and other credentials
+  allowedHeaders: 'Content-Type, Authorization', // Allow these headers
 };
 
 app.use(cors(corsAllow));
